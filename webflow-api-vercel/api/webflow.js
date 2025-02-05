3e89ccfc05a47fecf5b339dfe374ae4fd985a865
Ivan Vucetic <ivucetic7792v@gmail.com>
2025-02-05 10:32:53 +0100

    test commit

--- webflow-api-vercel/api/webflow.js
+++ webflow-api-vercel/api/webflow.js
@@ -1,3 +1,3 @@
-export default async function handler(req, res) {
-    res.status(200).json({ message: "✅ API is working!" });
+export default function handler(req, res) {
+    res.status(200).json({ message: "✅ API is now recognized!" });
 }
